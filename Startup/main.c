8a5a5432634061646d8a45fad1052054d5fb5d89
Khaled <khaledhasni8@gmail.com>
2024-03-07 01:46:32 +0100

    Fully integrated FreeRTOS

--- Startup/main.c
+++ Startup/main.c
@@ -1,7 +1,26 @@
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+static TaskHandle_t pvTaskHandle;
+
+static void vidTask0_Function(void *pvParam)
+{
+    /* Set breakpoint here */
+    __NOP();
+
+    /* Task function's main busy loop */
+    while(1)
+    {
+
+    }
+}
 
 int main(void)
 {
+    /* Create generic task */
+    xTaskCreate(vidTask0_Function, "Task0", 256, NULL, 2, &pvTaskHandle);
+
     /* Start Scheduler */
     vTaskStartScheduler();
 
