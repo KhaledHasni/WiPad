e71cbd1207b88b845264438f9e1461e44f85543d
Khaled <khaledhasni8@gmail.com>
2024-03-09 00:28:29 +0100

    Functional SW timer

--- Startup/main.c
+++ Startup/main.c
@@ -1,8 +1,10 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 
 static TaskHandle_t pvTaskHandle;
+static TimerHandle_t pvTimerHandle;
 
 static void vidTask0_Function(void *pvParam)
 {
@@ -12,14 +14,24 @@ static void vidTask0_Function(void *pvParam)
     /* Task function's main busy loop */
     while(1)
     {
-
     }
+}
+
+static void vidTimerCallback(TimerHandle_t pvTimerHandle)
+{
+    __NOP();
 }
 
 int main(void)
 {
     /* Create generic task */
     xTaskCreate(vidTask0_Function, "Task0", 256, NULL, 2, &pvTaskHandle);
+
+    /* Create timer */
+    pvTimerHandle = xTimerCreate("Timer0", pdMS_TO_TICKS(1000), pdTRUE, NULL, vidTimerCallback);
+
+    /* Start Timer */
+    BaseType_t lErrorCode = xTimerStart(pvTimerHandle, 0);
 
     /* Start Scheduler */
     vTaskStartScheduler();
