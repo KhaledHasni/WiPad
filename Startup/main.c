ee5b06d360f372a6e1978869bda993fd421c2c06
Khaled <khaledhasni8@gmail.com>
2024-03-12 20:17:59 +0100

    SW timers working properly

--- Startup/main.c
+++ Startup/main.c
@@ -1,19 +1,29 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "event_groups.h"
 #include "timers.h"
 
+#define WIPAD_MOCK_EVENT (1 << 0)
+
 static TaskHandle_t pvTaskHandle;
+static EventGroupHandle_t pvEventGroupHandle;
 static TimerHandle_t pvTimerHandle;
 
 static void vidTask0_Function(void *pvParam)
 {
-    /* Set breakpoint here */
-    __NOP();
+    EventBits_t u32Event = 0;
 
     /* Task function's main busy loop */
     while(1)
     {
+        /* Pull for synch events set in event group */
+        u32Event = xEventGroupWaitBits(pvEventGroupHandle, WIPAD_MOCK_EVENT, pdTRUE, pdTRUE, portMAX_DELAY);
+
+        if(u32Event)
+        {
+            __NOP();
+        }
     }
 }
 
@@ -26,6 +36,9 @@ int main(void)
 {
     /* Create generic task */
     xTaskCreate(vidTask0_Function, "Task0", 256, NULL, 2, &pvTaskHandle);
+
+    /* Create event group */
+    pvEventGroupHandle = xEventGroupCreate();
 
     /* Create timer */
     pvTimerHandle = xTimerCreate("Timer0", pdMS_TO_TICKS(1000), pdTRUE, NULL, vidTimerCallback);
