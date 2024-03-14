904df38fc178a952b94844ed6364369ca14fccf2
imen-matmati <imen.matmati@istic.ucar.tn>
2024-03-15 00:15:40 +0100

    clean integration

--- Middleware/Services/BLE_Service/BLE_Service.c
+++ Middleware/Services/BLE_Service/BLE_Service.c
@@ -1,15 +1,43 @@
+#include <string.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "event_groups.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
+#include "ble_gap.h"
+#include "nrf_ble_gatt.h"
 
 static TaskHandle_t pvBLETaskHandle;
 static EventGroupHandle_t pvBLEEventGroupHandle;
 
 #define WIPAD_MOCK_EVENT (1 << 0)
+NRF_BLE_GATT_DEF(m_gatt); 
+
+static void EventHandle(ble_evt_t const * p_ble_evt, void * p_context)
+{
+
+}
 
 static void vidBLE_Task_Function(void *pvParam)
 {
     EventBits_t u32Event = 0;
+    uint32_t ram_start = 0;
+    ble_gap_conn_sec_mode_t sec_mode;
+    ble_gap_conn_params_t gap_conn_params;
+
+    nrf_sdh_enable_request();
+    nrf_sdh_ble_default_cfg_set(1, &ram_start);
+    nrf_sdh_ble_enable(&ram_start);
+    NRF_SDH_BLE_OBSERVER(nrfObserver, 3, EventHandle, NULL);
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+    sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *)"Nordic_HRM", strlen("Nordic_HRM") );
+    sd_ble_gap_appearance_set(833);
+
+    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
+    sd_ble_gap_ppcp_set(&gap_conn_params);
+
+    nrf_ble_gatt_init(&m_gatt, NULL);
 
     /* Task function's main busy loop */
     while (1)
@@ -21,6 +49,7 @@ static void vidBLE_Task_Function(void *pvParam)
         {
             __NOP();
         }
+
     }
 }
 
