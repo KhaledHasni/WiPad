401785cf6a7dca48a5334fc2308b115c51c8a981
imen-matmati <imen.matmati@istic.ucar.tn>
2024-03-15 21:31:49 +0100

    verify integration

--- Middleware/Services/BLE_Service/BLE_Service.c
+++ Middleware/Services/BLE_Service/BLE_Service.c
@@ -1,29 +1,45 @@
 #include <string.h>
 #include "FreeRTOS.h"
 #include "task.h"
-#include "event_groups.h"
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
 #include "ble_gap.h"
 #include "nrf_ble_gatt.h"
+#include "ble_advertising.h"
+#include "ble_conn_params.h"
 
 static TaskHandle_t pvBLETaskHandle;
-static EventGroupHandle_t pvBLEEventGroupHandle;
+static TaskHandle_t m_softdevice_task;
 
-#define WIPAD_MOCK_EVENT (1 << 0)
+
 NRF_BLE_GATT_DEF(m_gatt); 
+BLE_ADVERTISING_DEF(m_advertising);
 
 static void EventHandle(ble_evt_t const * p_ble_evt, void * p_context)
 {
 
 }
 
+void SD_EVT_IRQHandler(void)
+{
+    BaseType_t yield_req = pdFALSE;
+
+    vTaskNotifyGiveFromISR(m_softdevice_task, &yield_req);
+
+    /* Switch the task if required. */
+    portYIELD_FROM_ISR(yield_req);
+}
+
 static void vidBLE_Task_Function(void *pvParam)
 {
-    EventBits_t u32Event = 0;
     uint32_t ram_start = 0;
     ble_gap_conn_sec_mode_t sec_mode;
     ble_gap_conn_params_t gap_conn_params;
+    ble_advertising_init_t init;
+    ble_conn_params_init_t cp_init;
+
+    memset(&init, 0, sizeof(init));
+    memset(&cp_init, 0, sizeof(cp_init));
 
     nrf_sdh_enable_request();
     nrf_sdh_ble_default_cfg_set(1, &ram_start);
@@ -39,17 +55,19 @@ static void vidBLE_Task_Function(void *pvParam)
 
     nrf_ble_gatt_init(&m_gatt, NULL);
 
+    ble_advertising_init(&m_advertising, &init);
+    ble_advertising_conn_cfg_tag_set(&m_advertising, 1);
+
+    ble_conn_params_init(&cp_init);
+
+
     /* Task function's main busy loop */
     while (1)
     {
-        /* Pull for synch events set in event group */
-        u32Event = xEventGroupWaitBits(pvBLEEventGroupHandle, WIPAD_MOCK_EVENT, pdTRUE, pdTRUE, portMAX_DELAY);
+        /* Pull for synch BLE events */
+        nrf_sdh_evts_poll();
 
-        if (u32Event)
-        {
-            __NOP();
-        }
-
+        (void) ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     }
 }
 
@@ -58,5 +76,5 @@ void BLE_Service_Init(void)
 
     /* Create task for BLE service */
     xTaskCreate(vidBLE_Task_Function, "Task0", 256, NULL, 2, &pvBLETaskHandle);
-    pvBLEEventGroupHandle = xEventGroupCreate();
+
 }
